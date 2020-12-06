b4e6f1fece0928a4b65ed82fc12162b2d6a1e122
123 <1520619515@qq.com>
2020-12-06 19:01:41 +0800

    评论管理

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -7,43 +7,72 @@
           <el-breadcrumb-item>评论管理</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!--
+      1. 把数据列表当顶给表格data
+      2. 设计表格列
+      3. 给表格列绑定要渲染的数据字段
+          -->
       <el-table
         class="table-list"
-        :data="tableData"
+        :data="articles"
         style="width: 100%"
         stripe
       >
         <el-table-column
-          prop="date"
-          label="日期"
-          width="180">
+          prop="title"
+          label="标题">
         </el-table-column>
         <el-table-column
-          prop="name"
-          label="姓名"
-          width="180">
+          prop="total_comment_count"
+          label="总评论数">
         </el-table-column>
         <el-table-column
-          prop="address"
-          label="地址">
+          label="粉丝评论数据"
+          prop="fans_comment_count">
+        </el-table-column>
+        <el-table-column
+          label="评论状态">
+          <template slot-scope="scope">
+            {{ scope.row.comment_status ? '正常' : '关闭'}}
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="操作">
+          <template slot-scope="scope">
+            <!-- 开关组件绑定的数据是一个布尔值，它会根据布尔值的真假来决定开关状态 -->
+            <el-switch
+              v-model="scope.row.comment_status"
+              :disabled="scope.row.statusDisabled"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+              @change="onStatusChange(scope.row)"
+            >
+            </el-switch>
+          </template>
         </el-table-column>
       </el-table>
+      <!--
+       绑定页码
+       绑定每页大小
+       current-page 控制激活的页码，初始肯定是第 1 页
+       page-sizes 控制可选的每页大小
+      -->
       <el-pagination
         background
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
-        :current-page="currentPage4"
-        :page-sizes="[100, 200, 300, 400]"
-        :page-size="100"
+        :current-page.sync="page"
+        :page-sizes="[10, 20, 50, 100]"
+        :page-size.sync="pageSize"
         layout="total, sizes, prev, pager, next, jumper"
-        :total="400">
+        :total="totalCount">
       </el-pagination>
     </el-card>
   </div>
 </template>
 
 <script>
-  import { getArticles} from "@/api/article";
+  import { getArticles, updateCommentStatus } from "@/api/article";
 
   export default {
   name: 'CommentIndex',
@@ -52,24 +81,10 @@
   },
   data () {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }],
-      currentPage4: 4
+      articles: [], // 文章数据列表
+      totalCount : 0,// 总数据条数
+      pageSize : 10,
+      page : 1  // 当前激活的页码默认是1
     }
   },
   computed: {
@@ -82,16 +97,46 @@
   },
   methods: {
     handleSizeChange(val) {
-      console.log(`每页 ${val} 条`);
+      this.loadArticles(1, val)
     },
-    handleCurrentChange(val) {
-      console.log(`当前页: ${val}`);
+    handleCurrentChange(page) {
+      // 页码给变 加载指定数据
+      this.loadArticles(page)
     },
-    loadArticles(){
+    loadArticles(page = 1){
+      this.page = page
       getArticles({
-        response_type : 'comment'
+        response_type : 'comment',
+        page,
+        per_page : this.pageSize
       }).then(res => {
-        console.log(res.data)
+        const results = res.data.data.results
+        results.forEach(item => {
+          item.statusDisabled = false
+        })
+        this.articles  = results
+        this.totalCount = res.data.data.total_count
+      })
+    },
+    onStatusChange (article) {
+      // 禁用开关
+      article.statusDisabled = true
+
+      // 请求提交修改
+      updateCommentStatus(article.id.toString(), article.comment_status).then(res => {
+        // 启用开关
+        article.statusDisabled = false
+        this.$message({
+          type: 'success',
+          message: article.comment_status ? '开启文章评论状态' : '关闭文章评论状态'
+        })
+      }).catch(err => {
+        article.statusDisabled = false
+        this.$message({
+          type: 'warning',
+          message: article.comment_status ? '开启文章评论状态失败' : '关闭文章评论状态失败'
+        })
+        article.comment_status = !article.comment_status
       })
     }
   },
@@ -105,6 +150,6 @@
 
 <style scoped lang="less">
   .table-list{
-    padding-bottom: 30px;
+    margin-bottom: 30px;
   }
 </style>
